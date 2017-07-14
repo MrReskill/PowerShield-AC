4c60fa4029492824f6473819be395518d8a6c745
MrReskill <mrreskill@gmail.com>
2017-07-14 18:08:45 +0200

    Simple edit

---
+++ src/PowerShield/Manager/ListenersManager.php
@@ -0,0 +1,17 @@
+<?php
+namespace PowerShield;
+
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+use pocketmine\utils\TextFormat as T;
+use PowerShield\Manager\{ListenersManager, CommandsManager};
+
+class ListenersManager implements Listener {
+
+    public function __construct()
+    {
+        // REGISTER ALL LISTENERS
+    }
+
+}
